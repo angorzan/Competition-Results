7fa8c014055c2d5cd46049c25ea7c3b7b1a8acf7
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-03-23 14:02:13 +0100

    Climbers and routes done

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,13 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const socket = io();
-    // const counterMiddleware = ('../../js/visit_counter_middleware.js');
-    console.log(socket);
-    socket.emit('simpleOne');
-    socket.emit('window', {
-        width : window.innerWidth,
-        height : window.innerHeight,
-    });
 
-    let newuser = prompt("Please enter your name");
-    console.log(newuser);
+    function Climber(ClimberName, ClimberId, ClimberTotalPoints) {
+        this.name = ClimberName;
+        this.id = ClimberId;
+        this.totalPoints = ClimberTotalPoints;
+    };
+
+    let JonSnow =new Climber('JonSnow', 1, 0);
+    let JaimeLannister = new Climber('JaimeLannister', 2, 0);
+    let RamsayBolton = new Climber('RamsayBolton', 3, 0);
+    let AryaStark = new Climber('AryaStark', 4, 0);
+    let BrienneOfTarth = new Climber('BrienneOfTarth', 5, 0);
+    let DaenerysTargaryen = new Climber('DaenerysTargaryen', 6, 0);
+    let climbers = [];
+    climbers.push(JonSnow, JaimeLannister, RamsayBolton, AryaStark, BrienneOfTarth, DaenerysTargaryen);
+    console.log(climbers);
+
+    function Route (name, id, points){
+        this.name = name;
+        this.id = id;
+        this.points = points;
+    };
+
+    let Greywater = new Route('Greywater Watch', 1, 10);
+    let Wall = new Route('Wall', 2, 100);
+    let GoldLoaks = new Route("City Watch of King's Landing", 3, 50);
+    let Riverrun = new Route('Riverrun', 4, 40);
+    let Winterfell = new Route('Winterfell', 5, 80);
+    let routes = [];
+    routes.push(Greywater, Wall, GoldLoaks, Riverrun, Winterfell);
+    console.log(routes);
+
 });
