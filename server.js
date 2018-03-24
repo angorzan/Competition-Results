81c60dc2e0660446519944f8d96729eac73e983f
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-03-24 17:31:36 +0100

    routing done

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.get('/',(req, res) => {
     res.send('Express.js z angorzan!');
 });
 
+app.get('/sent',(req, res) => {
+    res.send('Thank you for your data!');
+});
+
+app.get('/rankings',(req, res) => {
+    res.send('Total points ranking');
+});
+
 app.listen(3000, ()	=>	{
     console.log('Serwer is listening on	http://localhost:3000');
 });
