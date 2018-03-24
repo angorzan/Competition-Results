c551cad660010fafb8b12b0c0bcd608a53f08639
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-03-25 00:24:15 +0100

    climbers & routes created on server

--- server.js
+++ server.js
@@ -3,6 +3,43 @@ const app = express ();
 const path = require('path');
 const bodyParser = require('body-parser');
 
+
+class Climber{
+    constructor(ClimberName, ClimberId, ClimberTotalPoints)
+    {
+        this.name = ClimberName;
+        this.id = ClimberId;
+        this.totalPoints = ClimberTotalPoints;
+    }
+};
+
+let JonSnow = new Climber('JonSnow', 1, 0);
+let JaimeLannister = new Climber('JaimeLannister', 2, 0);
+let RamsayBolton = new Climber('RamsayBolton', 3, 0);
+let AryaStark = new Climber('AryaStark', 4, 0);
+let BrienneOfTarth = new Climber('BrienneOfTarth', 5, 0);
+let DaenerysTargaryen = new Climber('DaenerysTargaryen', 6, 0);
+let climbers = [];
+climbers.push(JonSnow, JaimeLannister, RamsayBolton, AryaStark, BrienneOfTarth, DaenerysTargaryen);
+console.log(climbers);
+
+class Route {
+    constructor(name, id, points) {
+        this.name = name;
+        this.id = id;
+        this.points = points;
+    }
+};
+
+let CasterlyRock = new Route('CasterlyRock', 1, 10);
+let Wall = new Route('Wall', 2, 100);
+let Riverrun = new Route('Riverrun', 4, 40);
+let Winterfell = new Route('Winterfell', 5, 80);
+let routes = [];
+routes.push(CasterlyRock, Wall, Riverrun, Winterfell);
+console.log(routes);
+
+
 app.use(express.static(
     path.join(__dirname, './src/static')
     )
@@ -23,6 +60,8 @@ app.post('/sent', (req, res) => {
     console.log(name, route, time, isDisqualified);
     res.send('Thank you for your data!');
 });
+
+
 
 // app.get('/sent',(req, res) => {
 //     res.send('Thank you for your data!');
--- src/static/js/app.js
+++ src/static/js/app.js
@@ -28,40 +28,13 @@ document.addEventListener('DOMContentLoaded', () => {
         })
             .then(r => r.text())
             .then(data => {
+                // tu powinien być middleware do zliczania punktów?
+                //tu powinien być warunek do wyświetlania rankingu
                 console.log(data);
             });
     });
 
 
-    function Climber(ClimberName, ClimberId, ClimberTotalPoints) {
-        this.name = ClimberName;
-        this.id = ClimberId;
-        this.totalPoints = ClimberTotalPoints;
-    };
-
-    let JonSnow =new Climber('JonSnow', 1, 0);
-    let JaimeLannister = new Climber('JaimeLannister', 2, 0);
-    let RamsayBolton = new Climber('RamsayBolton', 3, 0);
-    let AryaStark = new Climber('AryaStark', 4, 0);
-    let BrienneOfTarth = new Climber('BrienneOfTarth', 5, 0);
-    let DaenerysTargaryen = new Climber('DaenerysTargaryen', 6, 0);
-    let climbers = [];
-    climbers.push(JonSnow, JaimeLannister, RamsayBolton, AryaStark, BrienneOfTarth, DaenerysTargaryen);
-    console.log(climbers);
-
-    function Route (name, id, points){
-        this.name = name;
-        this.id = id;
-        this.points = points;
-    };
-
-    let CasterlyRock = new Route('CasterlyRock', 1, 10);
-    let Wall = new Route('Wall', 2, 100);
-    let Riverrun = new Route('Riverrun', 4, 40);
-    let Winterfell = new Route('Winterfell', 5, 80);
-    let routes = [];
-    routes.push(CasterlyRock, Wall, Riverrun, Winterfell);
-    console.log(routes);
 
     let DisableCheck = document.querySelector('#isDisqualified');
     let select = document.querySelectorAll('select:not(#isDisqualified)');
