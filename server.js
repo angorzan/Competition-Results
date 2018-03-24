237e710591519557bd8d964dd4872e0ef08c338b
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-03-24 22:48:26 +0100

    Data from form parsed and post

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const app = express ();
 const path = require('path');
+const bodyParser = require('body-parser');
+
 app.use(express.static(
     path.join(__dirname, './src/static')
     )
 );
+app.use(bodyParser.urlencoded({
+    extended : true,
+}));
+app.use(bodyParser.json());
 
 app.get('/',(req, res) => {
     res.send('Express.js z angorzan!');
 });
 
+app.post('/sent', (req, res) => {
+    const {ClimberName, routeNumber, time, isDisqualified} = req.body;
+    console.log(ClimberName, routeNumber, time, isDisqualified);
+    res.send('Thank you for your data!');
+});
+
 app.get('/sent',(req, res) => {
     res.send('Thank you for your data!');
 });
+
 
 app.get('/rankings',(req, res) => {
     res.send('Total points ranking');
